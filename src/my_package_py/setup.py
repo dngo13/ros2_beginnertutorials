635b1fb89d508e58b33a26a34dfd4f3eea2ad29c
Diane <diane.ngo1321@gmail.com>
2026-05-07 18:58:06 +0000

    package creation from tutorial

---
+++ src/my_package_py/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_package_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    package_data={'': ['py.typed']},
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Diane',
+    maintainer_email='diane.ngo1321@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'my_node = my_package_py.my_node:main'
+        ],
+    },
+)
